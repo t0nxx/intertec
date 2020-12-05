2eed1b0c516f46b73ea46833c1233fb7c6831023
AbdElRahman Ahmad <DrMagnet0@AbdElRahmans-MacBook-Pro.local>
2020-12-05 14:55:36 +0200

    Fix missig styles in contact form

--- src/components/screens/pharamaceutical-establishment/contact-inforamtion/contact-information.tsx
+++ src/components/screens/pharamaceutical-establishment/contact-inforamtion/contact-information.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { Button, Card, Col, Container, Form, Row, Image } from "react-bootstrap";
 import "./contact-information.scss";
+import "../shared/shared.scss";
+
+import email from "../../../../assets/mail.png";
+import phone from "../../../../assets/phone.png";
 
 const ContactInformationComponent = () => {
   return (
@@ -12,22 +14,38 @@ const ContactInformationComponent = () => {
     <Card>
       <Card.Body>
         <Container>
-          <Row>
-            <Col>
-              <h5>Retrive Information Contact From Your Profile</h5>
-              <Row>
-                <Button variant="outline-success" size="lg" className="btn-circle" />
-
-                <h6>Yes</h6>
-
-                <Button variant="outline-success" size="lg" className="btn-circle" active />
-
-                <h6>No</h6>
-              </Row>
-            </Col>
-          </Row>
-
           <Form>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Retrieve information contact from your profile</Form.Label>
+                <div className="radioButtons">
+                  <div className="form-check">
+                    <label htmlFor="getContactData">
+                      <input
+                        name="getContactData"
+                        type="radio"
+                        id="getContactData"
+                        className="form-check-input"
+                      />
+                      Yes
+                      <i className="checkMark"> </i>
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <label htmlFor="getContactData2">
+                      <input
+                        name="getContactData"
+                        type="radio"
+                        id="getContactData2"
+                        className="form-check-input"
+                      />
+                      No
+                      <i className="checkMark"> </i>
+                    </label>
+                  </div>
+                </div>
+              </Form.Group>
+            </Form.Row>
             <Form.Row>
               <Form.Group as={Col} controlId="formGridEmail">
                 <Form.Label>
@@ -49,14 +67,20 @@ const ContactInformationComponent = () => {
                 <Form.Label>
                   Primary Email <span className="required">*</span>{" "}
                 </Form.Label>
-                <Form.Control type="text" placeholder=" Primary Email" />
+                <div className="inputWithIcon">
+                  <Form.Control type="text" placeholder="Primary Email" />
+                  <Image src={email} />
+                </div>
               </Form.Group>
 
               <Form.Group as={Col} controlId="formGridPassword">
                 <Form.Label>
                   Alternative Email <span className="required">*</span>{" "}
                 </Form.Label>
-                <Form.Control type="text" placeholder="  Alternative Email " />
+                <div className="inputWithIcon">
+                  <Form.Control type="text" placeholder="  Alternative Email " />
+                  <Image src={email} />
+                </div>
               </Form.Group>
             </Form.Row>
 
@@ -65,21 +89,27 @@ const ContactInformationComponent = () => {
                 <Form.Label>
                   Contact Number <span className="required">*</span>{" "}
                 </Form.Label>
-                <Form.Control type="text" placeholder=" +971 000 000 " />
+                <div className="inputWithIcon">
+                  <Form.Control type="text" placeholder=" +971 000 000 " />
+                  <Image src={phone} />
+                </div>
               </Form.Group>
 
               <Form.Group as={Col} controlId="formGridPassword">
                 <Form.Label>
                   Alternative Contact Number <span className="required">*</span>{" "}
                 </Form.Label>
-                <Form.Control type="text" placeholder=" +971 000 000  " />
+                <div className="inputWithIcon">
+                  <Form.Control type="text" placeholder=" +971 000 000  " />
+                  <Image src={phone} />
+                </div>
               </Form.Group>
             </Form.Row>
 
             <Form.Row>
               <Form.Group as={Col} xs="6" controlId="formGridEmail">
                 <Form.Label> Fax </Form.Label>
-                <Form.Control type="text" placeholder="  " />
+                <Form.Control type="text" placeholder="Fax" />
               </Form.Group>
             </Form.Row>
           </Form>
