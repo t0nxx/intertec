4ffb3eb51439003480899892e82685b8ff955408
AbdElRahman Ahmad <DrMagnet0@AbdElRahmans-MacBook-Pro.local>
2020-12-05 16:20:59 +0200

    Finish footer icons

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,25 +1,21 @@
 import React from "react";
 import "./footer.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faMicrophone,
-  faPhoneAlt,
-  faQuestionCircle,
-  faSearch,
-  faSitemap,
-  faVolumeUp,
-  faLowVision,
-} from "@fortawesome/free-solid-svg-icons";
-import { Col, Dropdown, Nav, Navbar, Image, ProgressBar, Row } from "react-bootstrap";
+import { Navbar, Image, ProgressBar, Row, Button } from "react-bootstrap";
 
 // Import images
 import applicationIc from "../../assets/applicationIc.png";
 import attachmentIc from "../../assets/attachmentIc.png";
 import previewIc from "../../assets/previewIc.png";
+import info from "../../assets/info.png";
+import flag from "../../assets/flag.png";
 
 export default function FooterComponent() {
   return (
     <Navbar expand="lg" fixed="bottom" className="footer">
+      <div className="stickyIcons">
+        <Image src={info} className="infoIc" />
+        <Image src={flag} className="flagIc" />
+      </div>
       <ProgressBar variant="success" now={70} className="progress" />
       <Row className="statistics-container">
         <div>
