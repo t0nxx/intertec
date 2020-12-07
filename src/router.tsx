d7566d6bec014f2af351defb32caef9e3420ea00
t0nxx <mahmoudko1500@hotmail.com>
2020-12-07 21:33:28 +0200

    finish review route

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import AttachmentComponent from "./components/attachment/attachment";
+import ReviewComponent from "./components/review/review";
 import PharamaceuticalEstablishmentScreen from "./components/screens/pharamaceutical-establishment/pharamaceutical-establishment";
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={PharamaceuticalEstablishmentScreen} />
-      <Route exact path="/attachment" component={AttachmentComponent} />
-      {/* <Route exact path="/home" component={Homepage} /> */}
+      <Route path="/attachment" component={AttachmentComponent} />
+      <Route path="/review" component={ReviewComponent} />
     </Switch>
   );
 }
