fe7e749719e59d1cdf6f235a09ba6f5075129bdd
AbdElRahman Ahmad <DrMagnet0@AbdElRahmans-MacBook-Pro.local>
2020-12-09 23:30:13 +0200

    Finish info component

---
+++ src/components/sticky-info/sticky-info.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./sticky-info.scss";
+import { Button, Accordion, Card, Image } from "react-bootstrap";
+
+// Import image
+import upArrow from "../../assets/upArrow4Collaps.svg";
+import rightArrow from "../../assets/rightArrow.svg";
+
+export default function InfoComponent() {
+  return (
+    <div className="stickyInfo">
+      <div className="infoContent">
+        <Accordion defaultActiveKey="0">
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                Services Description
+                <Image src={upArrow} />
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                <p>
+                  Sample Text Lorem ipsum dummy text Sample Text Lorem ipsum dummy text Sample Text
+                  Lorem ipsum dummy text Sample Text Lorem ipsum dummy text Sample Text Lorem ipsum
+                  dummy text Sample Text Lorem ipsum dummy text Sample Text Lorem ipsum dummy text
+                  Sample Text Lorem ipsum dummy text Sample Text Lorem ipsum dummy text
+                </p>
+                <div className="showMore">
+                  show More
+                  <Image src={rightArrow} />
+                </div>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                Service fees
+                <Image src={upArrow} />
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body>
+                <ul>
+                  <li>Fee 1 (100 AED)</li>
+                  <li>Fee 2 (1000 AED)</li>
+                  <li>Fee 3 (500 AED)</li>
+                </ul>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      </div>
+      {/* <div className="infoIcon opened"> </div> */}
+      <div className="infoIcon"> </div>
+    </div>
+  );
+}
