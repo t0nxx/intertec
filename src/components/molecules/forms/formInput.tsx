8b0f950e0f39d11ad9498baade649a4bd865542e
t0nxx <mahmoudko1500@hotmail.com>
2020-12-16 14:16:34 +0200

    finish esablishment info with forminputs component

--- src/components/molecules/forms/formInput.tsx
+++ src/components/molecules/forms/formInput.tsx
@@ -2,37 +2,37 @@ import React from "react";
 import { Col, Form, FormGroup } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
-export const FormInputComponent = ({
-  label,
-  id,
-  name,
-  type,
-  register,
-  errors,
-  formState,
-  ...inputProps
+export const FormInputComponent = (props: {
+  label: string;
+  name: string;
+  type: string;
+  isRequird: boolean;
+  register: any;
+  as?: any;
+  errors: any;
+  formState: any;
+  children?: any;
 }) => {
   const { t }: any = useTranslation();
   return (
     <FormGroup as={Col} md="6" sm="12">
-      <label htmlFor={id}>{label}</label>
-      <input ref={register} id={id} name="password" type="password" />
-      {errors.password && <div>{errors.password.message}</div>}
-
       <Form.Label>
-        {t(`Forms.${label}`)} <span className="required">*</span>
+        {t(`Forms.${props.label}`)} {props.isRequird ? <span className="required">*</span> : null}
       </Form.Label>
       <Form.Control
-        placeholder={t(`Forms.${label}`)}
-        name={name}
-        type={type}
-        ref={register}
-        isValid={formState.touched && !errors.establishmentName}
-        isInvalid={errors.establishmentName}
-      />
-      {errors.establishmentName ? (
-        <span className="text-danger">{t(`${errors.establishmentName?.message}`)}</span>
-      ) : null}
+        placeholder={t(`Forms.${props.label}`)}
+        id={props.name}
+        name={props.name}
+        type={props.type}
+        as={props.as}
+        ref={props.register}
+        isValid={props.formState.touched[props.name] && !props.errors}
+        isInvalid={props.errors}
+      >
+        {/* this optional for like select menu ...etc */}
+        {props.children}
+      </Form.Control>
+      {props.errors ? <span className="text-danger">{t(`${props.errors?.message}`)}</span> : null}
     </FormGroup>
   );
 };
